0736b60fa20dcf89c0050028c5eb6d3887aedac7
jordanaf <jordanfranklinaf94@gmail.com>
2026-09-12 16:32:47 -0400

    mythic testing

--- src/pages/HuntingArea.tsx
+++ src/pages/HuntingArea.tsx
@@ -48,7 +48,7 @@ const leaderColumns = [
       const { area, areaSlug } = row.original
       if (areaSlug) {
         return (
-          <Link to={`/images/${areaSlug}.png`} className="text-ivory underline decoration-gilt/60 hover:decoration-gilt dark:text-ivory">
+          <Link to={`/public/images/${areaSlug}.png`} className="text-ivory underline decoration-gilt/60 hover:decoration-gilt dark:text-ivory">
             {area}
           </Link>
         )
