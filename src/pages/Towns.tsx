633cb7b0f6de16025125d46a581c793ae1e23b1e
jordanaf <jordanfranklinaf94@gmail.com>
2026-09-13 11:01:33 -0400

    updated path for town thumbnails

--- src/pages/Towns.tsx
+++ src/pages/Towns.tsx
@@ -1,17 +1,17 @@
 import { PageHeader } from '@/components/ui/PageHeader'
 import { CardGrid, type CardItem } from '@/components/ui/CardGrid'
 
-const thumbs = import.meta.env.BASE_URL + 'images/towns/thumbs/'
+const thumbs = import.meta.env.BASE_URL + 'images/towns/'
 
 const towns: CardItem[] = [
-  { title: 'Mileth', description: 'The starting town. Home to class trainers and Miraelis.', to: '/towns/mileth', image: `${thumbs}mileth.avif` },
-  { title: 'Abel', description: 'A coastal town with the fishing market and boat access.', to: '/towns/abel', image: `${thumbs}abel.webp` },
-  { title: 'Piet', description: 'Home to the Alchemy lab and Skandara\'s temple.', to: '/towns/piet', image: `${thumbs}piet.webp` },
-  { title: 'Loures', description: 'A grand city with connections to various areas.', to: '/towns/loures', image: `${thumbs}loures.webp` },
-  { title: 'Undine', description: 'Location of the Enchanting building.', to: '/towns/undine', image: `${thumbs}undine.webp` },
-  { title: 'Suomi', description: 'The Cherry Farmer\'s home. Serendael resides here.', to: '/towns/suomi', image: `${thumbs}suomi.webp` },
-  { title: 'Rucesion', description: 'Home to Jewelcrafting and Theselene\'s temple.', to: '/towns/rucesion', image: `${thumbs}rucesion.webp` },
-  { title: 'Tagor', description: 'Location of the Weaponsmithing forge.', to: '/towns/tagor', image: `${thumbs}tagor.webp` },
+  { title: 'Mileth', description: 'The starting town. Home to class trainers and Miraelis.', to: '/towns/mileth', image: `mileth.avif` },
+  { title: 'Abel', description: 'A coastal town with the fishing market and boat access.', to: '/towns/abel', image: `abel.avif` },
+  { title: 'Piet', description: 'Home to the Alchemy lab and Skandara\'s temple.', to: '/towns/piet', image: `piet.avif` },
+  { title: 'Loures', description: 'A grand city with connections to various areas.', to: '/towns/loures', image: `loures.avif` },
+  { title: 'Undine', description: 'Location of the Enchanting building.', to: '/towns/undine', image: `undine.avif` },
+  { title: 'Suomi', description: 'The Cherry Farmer\'s home. Serendael resides here.', to: '/towns/suomi', image: `suomi.avif` },
+  { title: 'Rucesion', description: 'Home to Jewelcrafting and Theselene\'s temple.', to: '/towns/rucesion', image: `rucesion.avif` },
+  { title: 'Tagor', description: 'Location of the Weaponsmithing forge.', to: '/towns/tagor', image: `tagor.avif` },
 ]
 
 export function Towns() {
