e84badfc8a47aa5195bd691a14d42260583205ee
jordanaf <jordanfranklinaf94@gmail.com>
2026-09-13 11:05:42 -0400

    path updating

--- src/pages/Towns.tsx
+++ src/pages/Towns.tsx
@@ -4,13 +4,13 @@ import { CardGrid, type CardItem } from '@/components/ui/CardGrid'
 const thumbs = import.meta.env.BASE_URL + 'images/towns/'
 
 const towns: CardItem[] = [
-  { title: 'Mileth', description: 'The starting town. Home to class trainers and Miraelis.', to: '/towns/mileth', image: `mileth.avif` },
-  { title: 'Abel', description: 'A coastal town with the fishing market and boat access.', to: '/towns/abel', image: `abel.avif` },
-  { title: 'Piet', description: 'Home to the Alchemy lab and Skandara\'s temple.', to: '/towns/piet', image: `piet.avif` },
-  { title: 'Loures', description: 'A grand city with connections to various areas.', to: '/towns/loures', image: `loures.avif` },
-  { title: 'Undine', description: 'Location of the Enchanting building.', to: '/towns/undine', image: `undine.avif` },
-  { title: 'Suomi', description: 'The Cherry Farmer\'s home. Serendael resides here.', to: '/towns/suomi', image: `suomi.avif` },
-  { title: 'Rucesion', description: 'Home to Jewelcrafting and Theselene\'s temple.', to: '/towns/rucesion', image: `rucesion.avif` },
+  { title: 'Mileth', description: 'The starting town. Home to class trainers and Miraelis.', to: '/towns/mileth', image: `${towns}mileth.avif` },
+  { title: 'Abel', description: 'A coastal town with the fishing market and boat access.', to: '/towns/abel', image: `${towns}abel.avif` },
+  { title: 'Piet', description: 'Home to the Alchemy lab and Skandara\'s temple.', to: '/towns/piet', image: `${towns}piet.avif` },
+  { title: 'Loures', description: 'A grand city with connections to various areas.', to: '/towns/loures', image: `${towns}loures.avif` },
+  { title: 'Undine', description: 'Location of the Enchanting building.', to: '/towns/undine', image: `${towns}undine.avif` },
+  { title: 'Suomi', description: 'The Cherry Farmer\'s home. Serendael resides here.', to: '/towns/suomi', image: `${towns}suomi.avif` },
+  { title: 'Rucesion', description: 'Home to Jewelcrafting and Theselene\'s temple.', to: '/towns/rucesion', image: `${towns}rucesion.avif` },
   { title: 'Tagor', description: 'Location of the Weaponsmithing forge.', to: '/towns/tagor', image: `tagor.avif` },
 ]
 
