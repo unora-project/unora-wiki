700aa977d41fb9f230793c3116c4548febb1df4f
jordanaf <jordanfranklinaf94@gmail.com>
2026-09-13 22:35:16 -0400

    description for dugon

--- src/pages/ClassDetail.tsx
+++ src/pages/ClassDetail.tsx
@@ -293,6 +293,13 @@ export function ClassDetail() {
       {/* Dugons Tab */}
       {activeTab === 'dugons' && dugons.length > 0 && (
         <section>
+          <p className="mb-4 text-center text-sm text-parchment-600 dark:text-parchment-400">
+      Dugons are an important part of the Monk class!  Start by heading to Sapphire Streams and speaking to Sabonim!
+
+      When you're ready say "Sabonim, Please teach me the -color- dugon."  (ie. Sabonim, Please teach me the White dugon.)
+
+      After meditating (which can take a bit of time), return to Sabonim and say "Sabonim, I understand the -color- dugon." (ie. Sabonim, I understand the White dugon.)
+    </p>
           <DataTable
             data={dugons}
             columns={dugonColumns}
